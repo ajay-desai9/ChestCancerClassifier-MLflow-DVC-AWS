a76412e9d4dd5e2efd5d051171d46ea6c0c8ccc5
ajay-desai9 <ajay.desai509@gmail.com>
2025-06-02 12:53:13 -0400

    updated main.py endpoint with base model preparation

--- main.py
+++ main.py
@@ -24,3 +24,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Base Model Preparation"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
